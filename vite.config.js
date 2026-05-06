2e2318f757eb3a7e6738de498f59ecb8a3e36d6a
Shah Aaryan <shaharyan408@gmail.com>
2026-05-06 16:58:10 +0530

    Added base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 'Todo-list' tamara GitHub repository nu naam che. 
+  // Aa line add karvi jaruri che.
+  base: '/Todo-list/', 
   plugins: [react()],
 })
